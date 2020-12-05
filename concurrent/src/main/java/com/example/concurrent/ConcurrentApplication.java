f2e516074ce51b0ea9a2f3e1cbc2d5bbeddc0d38
luoli@prolog-int.com <15527730396a>
2020-12-05 12:15:41 +0800

    第一次提交

---
+++ concurrent/src/main/java/com/example/concurrent/ConcurrentApplication.java
@@ -0,0 +1,16 @@
+package com.example.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author luoli
+ */
+@SpringBootApplication
+public class ConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrentApplication.class, args);
+    }
+
+}
