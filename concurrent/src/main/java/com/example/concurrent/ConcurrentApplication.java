36030471538005d207fdea91ccc3e2b64738e530
luoli@prolog-int.com <15527730396a>
2020-12-22 18:51:52 +0800

    测试提交

--- concurrent/src/main/java/com/example/concurrent/ConcurrentApplication.java
+++ concurrent/src/main/java/com/example/concurrent/ConcurrentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @author luoli
  */
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ConcurrentApplication {
